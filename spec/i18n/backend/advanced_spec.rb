c4d566090aff6d7eb550ddf4dd92e2033a115408
Michael Sievers <michael_sievers@web.de>
2015-12-01 16:31:00 +0100

    Made specs more narrative

--- spec/i18n/backend/advanced_spec.rb
+++ spec/i18n/backend/advanced_spec.rb
@@ -5,15 +5,24 @@ describe I18n::Backend::Advanced do
     end
   end
 
-  it "translates the given keys" do
-    [
-      ["application.cancel", "Abbrechen"],
-      ["users.index.cancel", "Abbrechen"],
-      ["users.index.languages.en", "Englisch"],
-      ["users.index.submit", "Abschicken"]
-    ]
-    .each do |_key, _value|
-      expect(backend.translate(:de, _key)).to eq(_value)
+  [
+    ["application.cancel", "Abbrechen"],
+    ["users.index.cancel", "Abbrechen"],
+    ["users.index.languages.en", "Englisch"],
+    ["users.index.submit", "Abschicken"],
+    ["users.index.greet", { user: "Max" }, "Hallo Max!"],
+    ["users.index.inbox", { count: 1 }, "Eine Nachricht"],
+    ["users.index.inbox", { count: 2 }, "2 Nachrichten"]
+  ]
+  .each do |_key, _options, _value|
+    if _options && _value.nil?
+      _value = _options
+      _options = {}
+    end
+
+    describe ["translate \"#{_key}\"", _options.empty? ? nil : _options].compact.join(", ") do
+      subject { backend.translate(:de, _key, _options) }
+      it { is_expected.to eq(_value) }
     end
   end
 end
